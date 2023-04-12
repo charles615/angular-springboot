d1cad484f1f384f66a019401433f10003c36ac72
Charles <22453341liu@gmail.com>
2023-04-12 17:15:06 +0800

    4
    4

---
+++ angular/src/app/klass/klass.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { IndexComponent } from './index.component';
+import { SharedModule } from '../shared/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { KlassAddComponent } from './klass-add/klass-add.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [ 
+  {
+    path: '',
+    component: IndexComponent
+  }, {
+    path: 'add',
+    component: KlassAddComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    IndexComponent,
+    KlassAddComponent
+  ],
+  imports: [
+    SharedModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class KlassModule { }
